70348262f251492ee82749f9e232b853a0d593a1
ivyne <francisca.ivyne@gmail.com>
2023-05-26 12:26:42 -0300

    término da 4

--- Trabalho Lógica da Programação/Exercicio03_EmpresaDeLogistica.py
+++ Trabalho Lógica da Programação/Exercicio03_EmpresaDeLogistica.py
@@ -1,9 +1,113 @@
 #Apresentando meu código identificador e meu nome
-id = ("\nCódigo Identificador: 4500068\nAluna: Francisca Ívyne Ripardo de Sousa\n\nExercicio 1 ")
-print(id)
+print("\nCódigo Identificador: 4500068\nAluna: Francisca Ívyne Ripardo de Sousa\n\nExercicio 3\n ")
 
 #Empresa de Logística
 print("Bem-vindo a Empresa de Logística da Ívyne Ripardo")
-#Valor por objeto é igual ao total= dimensões*peso*rota
-while True:
-  input()
+
+#Função da verificação de dimensões do objeto e o valor
+def dimensoesObjeto():
+  while True:
+    try:
+      print("\n","-"*25,"MEDIDAS","-"*30,"\n")
+      print("Atenção, ao invés de vírgula, utilizar ponto!!!")
+      largura = float(input("Digite a Largura do Objeto (cm):\n"))
+      comprimento = float(input("Digite o Comprimento do Objeto (cm):\n"))
+      altura = float(input("Digite a Altura do Objeto (cm):\n"))
+
+      cubico = altura * largura * comprimento
+      
+      #Verificando o valor do cm³ e se é aceito
+      if (cubico < 1000):
+        return 10
+      elif (1000 <= cubico < 10000):
+        return 20
+      elif (10000 <= cubico < 30000):
+        return 30
+      elif (30000 <= cubico < 100000):
+        return 50
+      else:
+        print("-"*60,"\n")
+        print("Valor cúbico não aceito")
+        print("Dimensões grandes não são permitidas")
+        print("Tente Novamente!!!\n")
+        print("-"*60,"\n")
+        continue
+    except ValueError:
+      print("-"*60,"\n")
+      print("Letras e códigos não são aceitos")
+      print("Digite um valor númerico!!!!\n")
+      print("-"*60)
+      continue
+    
+#Função para calcular o multiplicador do peso    
+def pesoObjeto():
+  while True:
+    try:
+      print("\n","-"*28,"PESO","-"*28)
+      peso = float(input("Digite o peso do Objeto (Kg):\n"))
+      if (peso<=0.1):
+        return 1
+      elif (0.1 <= peso < 1):
+        return 1.5
+      elif (1 <= peso < 10):
+        return 2
+      elif (10 <= peso < 30):
+        return 3
+      else:
+        print("-"*60,"\n")
+        print("Peso não aceito")
+        print("Objeto acima do peso")
+        print("Digitar novamente o peso do Objeto (Kg)!!!\n")
+        print("-"*60,"\n")
+        continue
+    except ValueError:
+      print("-"*60,"\n")
+      print("Letras e códigos não são aceitos")
+      print("Digite um valor númerico!!!!\n")
+      print("-"*60)
+      continue 
+
+#Função para calcular o multiplicador da rota
+def rotaObjeto():
+  while True:
+      print("\n","-"*28,"ROTA","-"*28)
+      print("\n Código |      Rota")
+      print("   RS   | de Rio de Janeiro até São Paulo")
+      print("   SR   | de São Paulo até Rio de Janeiro")
+      print("   BS   | de Brasília até São Paulo ")
+      print("   SB   | de São Paulo até Brasília")
+      print("   BR   | de Brasília até Rio de Janeiro")
+      print("   RB   | de Rio de Janeiro até Brasília")
+      print("-"*60,"\n")
+
+      rota = input("Digite a sigla de sua rota:\n")
+      
+      if (rota=="RS" or rota=="rs"):
+        return 1
+      elif (rota=="SR" or rota=="sr"):
+        return 1
+      elif (rota=="BS" or rota=="bs"):
+        return 1.2
+      elif (rota=="SB" or rota=="sb"):
+        return 1.2
+      elif (rota=="BR" or rota=="br"):
+        return 1.5
+      elif (rota=="RB" or rota=="rb"):
+        return 1.5
+      else:
+        print("-"*60,"\n")
+        print("\nERRO - Rota não encontrada")
+        print("Digite novamente a sigla da rota que deseja!!!")
+        print("-"*60,"\n")
+        continue
+
+#Chamando as funções e calculando o total
+objeto = dimensoesObjeto()
+peso = pesoObjeto()
+rota = rotaObjeto()
+total = objeto*peso*rota
+
+#Nota fiscal, repassando valor total e cálculo
+print("\n","-"*22," Nota Fiscal ","-"*22,"\n")
+print("Total a pagar(R$){:.2f}\n\n(dimensões: R${:.2f} * peso:{} * rota:{})".format(total,objeto,peso,rota),"\n")
+print("-"*60)
--- Trabalho Lógica da Programação/Exercicio04_EstoqueBicicletaria.py
+++ Trabalho Lógica da Programação/Exercicio04_EstoqueBicicletaria.py
@@ -1,6 +1,126 @@
 #Apresentando meu código identificador e meu nome
-id = ("\nCódigo Identificador: 4500068\nAluna: Francisca Ívyne Ripardo de Sousa\n\nExercicio 4 ")
-print(id)
+print("\nCódigo Identificador: 4500068\nAluna: Francisca Ívyne Ripardo de Sousa\n\nExercicio 4 \n")
 
 #Empresa de Logística
 print("Bem-vindo ao Estoque da Bicicletaria da Ívyne Ripardo")
+
+#Variavel Global
+estoque_peca = []
+codigo_peca = 0
+
+
+#Função de Cadastro
+def cadastrarPeca(codigo):
+  print("-"*28,"CADASTRO","-"*28)
+  print("O código do produto: {}".format(codigo))
+  nome_peca = input("Digite o nome da peça: ")
+  fabricante = input("Digite o fabricante da peça: ")
+  valor_peca = float(input("Digite o valor da peça(R$): \n"))
+  #Dicionário das peças
+  dicionario_peca = {"Código": codigo,
+                     "Nome": nome_peca,
+                     "Fabricante:": fabricante,
+                     "Preço": valor_peca,}
+  print("Peça Cadastrada com Sucesso...\n")
+  #Incluindo uma cópia do dicionário na lista global
+  estoque_peca.append(dicionario_peca.copy())
+
+
+#Função de Consulta
+def consultarPeca():
+  #Criando um loop para caso a opção seja inválida
+  while True:
+    #try e except para verificar se a opcao é um número
+      print("-"*28,"CONSULTA DE PEÇA","-"*28,"\n")
+      print("1 - Consultar Todas as Peças ")
+      print("2 - Consultar Produto por Código")
+      print("3 - Consultar Produto por Fabricante")
+      print("4 - Retornar à Tela de Menu")
+      opcao = int(input("Digite a opção que deseja: "))
+
+      if (opcao == 1):
+        print("-"*28,"TODOS OS PRODUTOS","-"*28,"\n")
+        for peca in estoque_peca: #a variável peca vai varrer toda a lista de estoque
+          print("-"*20)
+          for key,value in peca.items(): #varrer a lista com a chave e seu valor
+            print("{} : {}".format(key,value))
+          print("-"*20)
+
+      elif (opcao == 2):
+        print("-"*28,"CONSULTA POR CÓDIGO","-"*28,"\n")
+        cod = int(input("Entre com o Código desejado: ")) #Pede o número do código
+        for peca in estoque_peca: #a variável peca vai varrer toda a lista de estoque
+          if peca["Código"] == cod: #Verifica se o Código está na lista
+            print("-"*20)
+            for key, value in peca.items(): #varrer a lista com a chave e seu valor
+              print("{} : {}".format(key,value))
+            print("-"*20)
+
+      elif (opcao == 3):
+        print("-"*28,"CONSULTA POR FABRICANTE","-"*28,"\n")
+        cod = int(input("Entre com o Fabricante: "))
+        for peca in estoque_peca: #a variável peca vai varrer toda a lista de estoque
+          if peca["Fabricante"] == cod: #Verifica se o Código está na lista
+            print("-"*20)
+            for key, value in peca.items(): #varrer a lista com a chave e seu valor
+              print("{} : {}".format(key,value))
+            print("-"*20)
+
+      elif (opcao == 4):
+        print("\nRetornando em 3...")
+        print("2...")
+        print("1...\n")
+        return #Sai da função consultarPeca() e volta para o main
+        
+      else: #Caso o valor não seja nenhuma das alternativas, ele retorna ao começo da função
+        print("\nERRO, VALOR INVÁLIDO")
+        print("Digite um número das opções!!!\n")
+        continue
+
+
+#Função de Remover 
+def removerPeca():
+    print("-"*28,"EXCLUIR PRODUTOS","-"*28,"\n")
+    cod = int(input("Entre com o código da peça que deseja remover: "))
+    for peca in estoque_peca:
+      if peca["Código"] == cod:
+        estoque_peca.remove(peca)
+        print("Produto Removido!!!")
+
+#                     MAIN
+
+#                     MENU
+
+#Criando um loop para caso a opção seja inválida
+while True:
+  #try e except para verificar se a opcao é um número
+  try:
+    print("-"*28,"MENU","-"*28,"\n")
+    print("1 - Cadastrar Peça")  
+    print("2 - Consultar Peça")
+    print("3 - Remover Peça")
+    print("4 - sair\n")  
+    opcao = int(input("Digite a opção que deseja: "))
+
+
+    if (opcao == 1):
+      codigo_peca = codigo_peca + 1
+      cadastrarPeca(codigo_peca)
+    elif (opcao == 2):
+      consultarPeca()
+    elif (opcao == 3):
+      removerPeca()
+    elif (opcao == 4):
+      print("\nEncerrando o sistema em 3..")
+      print("2...")
+      print("1...")
+      print("Encerrado")
+      break
+    else:
+      print("\nERRO, VALOR INVÁLIDO")
+      print("Digite um número das opções!!!\n")
+      continue #Volta para o inicio do Menu
+  except ValueError:
+    print("\nERRO, VALOR INVÁLIDO")
+    print("Digite um número das opções!!!\n")
+    continue #Volta para o inicio do Menu
